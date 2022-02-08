c5a2783223eab88711b0757ed56e167bac6ada6a
Shane O'Moore <shaneiadt@gmail.com>
2022-02-08 19:01:13 +0000

    navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+import icon from '../../images/logo.png';
 
 export const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Cryptoverse</Link>
+        </Typography.Title>
+        {/* <Button className='menu-control-container'></Button> */}
+      </div>
+    </div>
+  );
 };
