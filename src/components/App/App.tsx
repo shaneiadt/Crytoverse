c044fca44b50a1294f002bb181cb293808b251a8
Shane O'Moore <shaneiadt@gmail.com>
2022-02-09 14:16:04 +0000

    basic App component structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,17 +2,40 @@ import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from '../index';
+import { Navbar, HomePage, Exchanges, Currencies, Details, News } from '../index';
+import './App.css';
 
 const App = () => {
   return (
-  <div className='app'>
-    <div className="navbar">
-      <Navbar />
-    </div>
-    <div className="main"></div>
-    <div className="footer"></div>
-  </div>);
+    <div className='app'>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/exchanges' element={<Exchanges />} />
+              <Route path='/cryptocurrencies' element={<Currencies />} />
+              <Route path='/crypto/:coinId' element={<Details />} />
+              <Route path='/news' element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer">
+        <Typography.Title level={5} style={{ color: '#fff', textAlign: 'center' }}>
+          Cryptoverse <br />
+          All rights reserved
+        </Typography.Title>
+        <Space>
+          <Link to='/'>Home</Link>
+          <Link to='/exchanges'>Exchanges</Link>
+          <Link to='/news'>News</Link>
+        </Space>
+      </div>
+    </div>);
 }
 
 export default App;
