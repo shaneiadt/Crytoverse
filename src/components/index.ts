2ab1e3a584a3a23d0fa1e7e79f4b7d4c5911c1a9
Shane O'Moore <shaneiadt@gmail.com>
2022-02-09 14:15:43 +0000

    export all components in dir

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,6 @@
 export { Navbar } from './Navbar/Navbar';
+export { HomePage } from './HomePage/HomePage';
+export { Currencies } from './Crypto/Currencies/Currencies';
+export { Details } from './Crypto/Details/Details';
+export { News } from './News/News';
+export { Exchanges } from './Exchanges/Exchanges';
