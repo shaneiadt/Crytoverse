9459572c87904e69bb986f06106963a0d2c920fe
Shane O'Moore <shaneiadt@gmail.com>
2022-02-10 17:11:49 +0000

    useGetCryptosQuery hook

--- src/components/Crypto/Currencies/Currencies.tsx
+++ src/components/Crypto/Currencies/Currencies.tsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input } from 'antd';
 
-export const Currencies = () => {
+import { Coin, useGetCryptosQuery } from '../../../services/cryptoApi';
+
+interface ICurrenciesProps {
+  simplified?: boolean;
+}
+
+export const Currencies: React.FC<ICurrenciesProps> = ({ simplified = false }) => {
+  const count = simplified ? 10 : 100;
+  const { data } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState(data?.data?.coins);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const filteredData = data?.data?.coins.filter((coin: Coin) => coin.name.toLocaleLowerCase().includes(searchTerm.toLowerCase()));
+
+    setCryptos(filteredData);
+  }, [data, searchTerm]);
+
   return (
-    <div>Currencies</div>
+    <>
+      <div className="search-crypto">
+        <Input placeholder='Search Crypto Currency' value={searchTerm} onChange={({ target: { value } }) => setSearchTerm(value)} />
+      </div>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map(({ uuid, rank, name, iconUrl, price, marketCap, change }) => (
+          <Col xs={24} sm={12} lg={6} key={uuid}>
+            <Link to={`/crypto/${uuid}`}>
+              <Card title={`${rank}. ${name}`} hoverable extra={<img height="30" alt={`${name}`} className='crypto-image' src={iconUrl} />}>
+                <p>Price: {millify(Number(price))}</p>
+                <p>Market Cap: {millify(Number(marketCap))}</p>
+                <p>Daily Change: {millify(Number(change))}</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import millify from 'millify';
+import { Typography, Row, Col, Statistic, Spin } from 'antd';
+
+import { useGetCryptosQuery } from '../../services/cryptoApi';
+import { Link } from 'react-router-dom';
+import { Currencies, News } from '..';
+
+const { Title } = Typography;
 
 export const HomePage = () => {
+  const { data, isFetching } = useGetCryptosQuery(10);
+  const globalStats = data?.data?.stats;
+
+  if (isFetching) {
+    return (
+      <Row>
+        <Col span={24} style={{ display: 'flex', justifyContent: 'center' }}>
+          <Spin size='large' />
+        </Col>
+      </Row>
+    );
+  }
+
   return (
-    <div>HomePage</div>
+    <>
+      <Title level={2} className="heading">Global Crypto Stats</Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value={globalStats?.total} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value={millify(Number(globalStats?.totalExchanges))} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" value={millify(Number(globalStats?.totalMarketCap))} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h Volume" value={millify(Number(globalStats?.total24hVolume))} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value={millify(Number(globalStats?.totalMarkets))} />
+        </Col>
+      </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
+      </div>
+      <Currencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
+      </div>
+      <News simplified />
+    </>
   )
 }
