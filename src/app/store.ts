b659ff44668530f95738a53306fa469b0dacf984
Shane O'Moore <shaneiadt@gmail.com>
2022-02-11 14:04:37 +0000

    additional crypto news reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,14 @@
-import { configureStore, ThunkAction, Action, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 
 import { cryptoApi } from '../services/cryptoApi';
+import { cryptoNewsApi } from '../services/cryptoNewsApi';
 
 export const store = configureStore({
   reducer: {
-    [cryptoApi.reducerPath]: cryptoApi.reducer
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
+    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(cryptoApi.middleware)
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(cryptoApi.middleware, cryptoNewsApi.middleware)
 });
 
 export type AppDispatch = typeof store.dispatch;
