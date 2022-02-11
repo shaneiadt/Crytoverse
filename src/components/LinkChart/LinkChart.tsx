f65686fe9175f717e813c4d0cefb8b8e648bb5d6
Shane O'Moore <shaneiadt@gmail.com>
2022-02-11 16:08:46 +0000

    line chart

--- src/components/LinkChart/LinkChart.tsx
+++ src/components/LinkChart/LinkChart.tsx
@@ -1,10 +1,73 @@
 import { Line } from "react-chartjs-2";
 import { Col, Row, Typography } from 'antd';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
 
-const { Title } = Typography;
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
-export const LinkChart = () => {
+interface ILinkChartProps {
+    coinHistory: {
+        change: string;
+        history: { price: string; timestamp: number }[]
+    };
+    currentPrice: string;
+    coinName: string;
+}
+
+export const LinkChart: React.FC<ILinkChartProps> = ({ coinHistory, currentPrice, coinName }) => {
+    const coinPrice = [];
+    const coinTimeStamp = [];
+
+    for (let i = 0; i < coinHistory.history.length; i++) {
+        coinPrice.push(coinHistory.history[i].price);
+        coinTimeStamp.push(new Date(coinHistory.history[i].timestamp).toLocaleDateString());
+    }
+
+    const options = {
+        plugins: {
+            legend: {
+                position: 'top' as const,
+            },
+        },
+    };
+
+    const data = {
+        labels: coinTimeStamp,
+        datasets: [{
+            label: 'Price in USD',
+            data: coinPrice,
+            fill: false,
+            backgroundColor: '#0071bd',
+            borderColor: '#0071bd',
+        }]
+    }
+
     return (
-        <div>LinkChart</div>
+        <>
+            <Row className="chart-header">
+                <Typography.Title level={2} className='chart-title'>{coinName} Price Chart</Typography.Title>
+                <Col className="price-container">
+                    <Typography.Title level={5} className='price-change'>{coinHistory.change}%</Typography.Title>
+                    <Typography.Title level={5} className='current-price'>Current {coinName} ${currentPrice}</Typography.Title>
+                </Col>
+            </Row>
+            <Line data={data} options={options} />
+        </>
     )
 }
